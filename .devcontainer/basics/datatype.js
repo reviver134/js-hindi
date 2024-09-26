d8948ed24210e939b8196d832f69002286100af2
jayant dubey <jayantdubey25@gmail.com>
2024-09-26 04:51:36 +0000

    conversion_of_datatype

---
+++ .devcontainer/basics/datatype.js
@@ -0,0 +1,26 @@
+// let score = "jayant";
+
+// console.log(typeof (score));
+
+// // data types have capital formate
+// let valueInNumber = Number(score)
+// console.log((valueInNumber));
+
+// // "33" => 33
+// // // "33abc" => NaN
+// // // true =>1; false =>0
+
+// let isLoggedIn="";
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+// // 1 => true; 0 =>false
+// // "" => false
+// // "jayant" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber);
+console.log(stringNumber)
+console.log(typeof(stringNumber))
