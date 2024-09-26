72a59b469bf9839b507e8e763ecd6b8ccd4f6cb3
jayant dubey <jayantdubey25@gmail.com>
2024-09-26 04:21:03 +0000

    datatypes

--- .devcontainer/basics/test.js
+++ .devcontainer/basics/test.js
@@ -1,1 +1,21 @@
 console.log("jayant")
+"use strict"; // treat all js code as newer version
+//alert("hello") //we are using nodejs,not browser
+
+let name="jayant";
+let age=18;
+let isloggedin=true;
+
+
+// number => 2 to power 53
+// bigint
+// string =>""
+// boolean (0 or 1) (true/false)
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+console.log(typeof "jayant");
+console.log(typeof null);
